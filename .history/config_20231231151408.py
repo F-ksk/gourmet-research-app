0b382cb46e6568d71aecf8be67b63ce1a530cfce
F-ksk <kfutamata22@gmail.com>
2023-12-31 15:17:04 +0900

    update

---
+++ .history/config_20231231151408.py
@@ -0,0 +1,26 @@
+API_KEY = '5947c67fce02336b'
+geo_key = 'AIzaSyDM8T4eq6mVMGbt6tICq2CkK9Dw_SlGxDk'
+
+# vscode 1.85.1
+# Python 3.11.3
+# Windows, macOS, Linux
+
+#環境の作成
+# py -3 -m venv .venv
+
+# 環境をアクティブ化
+# .venv/Scripts/Activate.ps1
+
+# 実行
+# flask run --debug
+
+
+
+# アプリ公開 (heroku)
+# https://algorithm.joho.info/programming/python/flask-heroku/
+
+# 更新
+# git add .
+# git commit -m "update"
+# git push origin main
+# git push heroku main
